5c5e699eab75e425952c8c4260518f2a662bdbc6
april sikes <aprilsikes@gmail.com>
2016-02-22 15:43:39 -0700

    separating files

--- index.js
+++ index.js
@@ -1,102 +1,87 @@
-var app = angular.module('april', ['angularMoment']);
-
-app.controller('MyController', function ($scope) {
-  $scope.posts = [];
-  $scope.showForm = false;
-  $scope.showNew = false;
-  $scope.buttonValue = '0 comments';
-  $scope.orderProp = 'title';
-  $scope.sortReverse  = false;
-  // $scope.showNew = {};
-  console.log($scope);
-
-  $scope.upVote = function(post){
-    post.votes++;
-    post.votes.push(this.vote);
-  };
-  $scope.downVote = function(post){
-    post.votes--;
-    post.votes.push(this.vote);
-  };
-
-  $scope.toggleForm = function () {
-    $scope.showForm = !$scope.showForm;
-  }
-
-  $scope.toggleComments = function (post) {
-    this.showComments = !this.showComments;
-  }
-
-  $scope.toggleCommentForm = function (post) {
-    this.showNew = !this.showNew;
-  }
-
-  $scope.addPost = function () {
-    var post = {};
-    post.message = {
-      time: new Date()
-    };
-    post.image = $scope.image;
-    post.title = $scope.title;
-    post.author = $scope.author;
-    post.description = $scope.description;
-    post.comments = [];
-    post.votes = 0;
-    if (post.votes > 0) {
-      color: green;
-    } else if (post.votes < 0) {
-      color: red;
-    }
-    $scope.posts.push(post);
-    console.log($scope.posts);
-    this.showComments = false;
-    post.showNew = false;
-    $scope.toggleForm();
-    $scope.toggleComments();
-    $scope.title = null;
-    $scope.author = null;
-    $scope.image = null;
-    $scope.description = null;
-  }
-
-  $scope.addComment = function (post, author, text) {
-    var comment = {};
-    post.comments.author = this.author;
-    post.comments.text = this.text;
-    post.comments.push(this.comment);
-    console.log(post.comments);
-    this.showComments = false;
-    this.toggleCommentForm();
-    this.toggleComments();
-    // $scope.author = null;
-    // $scope.text = null;
-    // this.comment = {};
-    if (post.comments.length > 1) {
-      this.buttonValue = post.comments.length+ ' comments';
-    } else if (post.comments.length == 1) {
-      this.buttonValue = post.comments.length+ ' comment';
-    }
-  }
-
-
-
-})
-
-// NOTES:
-// use "this" to control scope (access the specific posts and comments you want)
-// in forms, use dot notation to create your post and comment objects on the scope
-
-
-
-// angular.module('timeApp', ['angularMoment']);
+// var app = angular.module('april', ['angularMoment']);
 //
-// // create an angular controller
-// .controller('mainController', function() {
+// app.controller('MyController', function ($scope) {
+//   $scope.posts = [];
+//   $scope.showForm = false;
+//   $scope.showNew = false;
+//   $scope.buttonValue = '0 comments';
+//   $scope.orderProp = 'title';
+//   $scope.sortReverse  = true;
+//   // $scope.showNew = {};
+//   console.log($scope);
 //
-//   // bind the controller to vm (view-model)
-//   var vm = this;
+//   $scope.upVote = function(post){
+//     post.votes++;
+//   };
+//   $scope.downVote = function(post){
+//     post.votes--;
+//   };
 //
-//   // create a new time variable with the current date
-//   vm.time = new Date();
+//   $scope.toggleForm = function () {
+//     $scope.showForm = !$scope.showForm;
+//   }
 //
-// });
+//   $scope.toggleComments = function (post) {
+//     this.showComments = !this.showComments;
+//   }
+//
+//   $scope.toggleCommentForm = function (post) {
+//     this.showNew = !this.showNew;
+//   }
+//
+//   $scope.changeColor = function (post) {
+//     if (post.votes < 0) {
+//       return 'red';
+//     } else if (post.votes > 0) {
+//       return 'green';
+//     } else {
+//       return 'black';
+//     }
+//   }
+//
+//   $scope.addPost = function () {
+//     var post = {};
+//     post.message = {
+//       time: new Date()
+//     };
+//     post.image = $scope.image;
+//     post.title = $scope.title;
+//     post.author = $scope.author;
+//     post.description = $scope.description;
+//     post.comments = [];
+//     post.votes = 0;
+//     $scope.posts.push(post);
+//     console.log($scope.posts);
+//     this.showComments = false;
+//     post.showNew = false;
+//     $scope.toggleForm();
+//     $scope.toggleComments();
+//     $scope.title = null;
+//     $scope.author = null;
+//     $scope.image = null;
+//     $scope.description = null;
+//   }
+//
+//   $scope.addComment = function (post, author, text) {
+//     var comment = {};
+//     comment.author = author;
+//     comment.text = text;
+//     post.comments.push(comment);
+//     console.log(post.comments);
+//
+//     post.showComments = false;
+//     this.toggleCommentForm(post);
+//     this.toggleComments(post);
+//     // $scope.author = null;
+//     // $scope.text = null;
+//     // this.comment = {};
+//     if (this.post.comments.length > 1) {
+//       this.buttonValue = this.post.comments.length+ ' comments';
+//     } else if (this.post.comments.length == 1) {
+//       this.buttonValue = this.post.comments.length+ ' comment';
+//     }
+//   }
+//
+//
+//
+// })
